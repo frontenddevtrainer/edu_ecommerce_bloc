5ce49212c406cb9d518f3f85c7291a689cdf1757
admin <admin@example.com>
2023-04-06 05:33:54 +0100

    update base url

--- lib/repository/authentication.dart
+++ lib/repository/authentication.dart
@@ -1,21 +1,24 @@
 import 'package:dio/dio.dart';
 import 'package:edu_ecommerce_bloc/models/user.dart';
+import 'package:flutter/foundation.dart';
+import "dart:io" show Platform;
 
 class AuthenticationRepository {
-  final dio = Dio();
 
-  @override
+  static final String _baseUrl = kReleaseMode ? "https://edureka.co" : (Platform.isAndroid ? "http://10.0.2.2:3000" : "http://localhost:3000");
+
+  static final dio = Dio(BaseOptions(baseUrl: _baseUrl));
+
   User? currentUser() {
     // throw UnimplementedError();
     return User(name: "praveen", id: "1");
   }
 
-  @override
   Future<User> login(String email, String password) async {
     print(email);
     print(password);
     try {
-      final response = await dio.post("http://example.com/login",
+      final response = await dio.post("/login",
           data: {email: email, password: password});
       return User(name: response.data["name"], id: response.data["id"]);
     } catch (e) {
@@ -23,17 +26,15 @@ class AuthenticationRepository {
     }
   }
 
-  @override
   Future<User> logout() {
     throw UnimplementedError();
   }
 
-  @override
   Future<User> signup(String email, String password) async {
     print(email);
     print(password);
     try {
-      final response = await dio.post("http://localhost:3000/signup",
+      final response = await dio.post("/signup",
           data: {"email": email, "password": password});
           print(response);
       return User(name: response.data["name"], id: response.data["id"]);
